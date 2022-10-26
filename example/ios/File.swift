0b003ef81630c5b975ef06d5447d176db7baa333
Aftab Baig <aftabaig@gmail.com>
2022-10-26 19:26:09 +0500

    First commit

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  MyLibraryExample
-//
-
-import Foundation
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    MyLibrary.multiply(3, 7).then(setResult);
+    MyLibrary.multiply(5, 7).then(setResult);
   }, []);
 
   return (
